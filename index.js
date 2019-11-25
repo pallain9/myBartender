836c73c710019220eae18eddb3c08faf1a937b63
Peter allain <pballain910@gmail.com>
2019-11-24 23:02:23 -0500

    updated project

--- index.js
+++ index.js
@@ -1,99 +1,46 @@
 const Alexa = require('ask-sdk-core')
 const axios = require('axios')
+const skillBuilder = Alexa.SkillBuilders.custom();
 
-/*
-"name": "AMAZON.FallbackIntent"
-"name": "AMAZON.CancelIntent"
-"name": "AMAZON.HelpIntent"
-"name": "AMAZON.StopIntent"
-"name": "AMAZON.NavigateHomeIntent"
-"name": "RecipeIntent" 
-"name": "AlcoholDrinkIntent"
-"name": "RandomDrinkIntent"
-"name": "IngredientIntent"
-*/
 
-async function getDrinkRecipe(drink, key) {
-    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`
-    const response = await axios.get(url)
+async function getInsult() {
+    const response = await axios.get(`https://evilinsult.com/generate_insult.php?lang=en&type=json`)
 
-    return response.data[key]
+    return response.data.insult
 }
-
-async function getRandomDrink(drink, key) {
-    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`
-    const response = await axios.get(url)
-
-    return response.data[key]
-}
-
-async function getIngredients(drink, key) {
-    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`
-    const response = await axios.get(url)
-
-    return response.data[key]
-}
-
-
-const RecipeIntentHandler = {
+const LaunchRequestHandler = {
     canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-            && handlerInput.requestEnvelope.request.intent.name === 'RecipeIntent'
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest'
     },
-    async handle(handlerInput) {
-        const drink = handlerInput.requestEnvelope.request.intent.slots.strDrink.value
-        const recipe = await getDrinkRecipe(drink, 'strInstructions')
-
-        const answer = `${drink}, excellent choice.  Are you ready?  ${recipe}.`
-        const reprompt = 'Would you like another drink?'
+    handle(handlerInput) {
+        const homeText = "Looking for something to bring you down a peg?  You have come to the right place!  Would you like an insult?"
+        const reprompt = 'I am begging you, let me dis you!'
 
         return handlerInput.responseBuilder
-            .speak(answer + reprompt)
+            .speak(homeText + reprompt)
+            .reprompt(reprompt)
+            .withShouldEndSession(false)
+            .getResponse()
+    }
+}
+
+const InsultIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'InsultIntent'
+    },
+    async handle(handlerInput) {
+        const insult = await getInsult()
+        const reprompt = 'Would you like another insult?'
+
+        return handlerInput.responseBuilder
+            .speak(insult)
             .reprompt(reprompt)
             .withShouldEndSession(false)
             .getResponse()
     },
 }
 
-/*const RandomDrinkIntentHandler = {
-    canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-            && handlerInput.requestEnvelope.request.intent.name === 'RecipeIntent'
-    },
-    async handle(handlerInput) {
-        const randomDrink = handlerInput.requestEnvelope.request.intent.slots.strDrink.value
-        const recipe = await getDrinkRecipe (randomDrink, 'strInstructions')
-
-        const answer = `${drink}, excellent choice.  Are you ready?  ${recipe}.`
-        const reprompt = 'Would you like another drink?'
-
-        return handlerInput.responseBuilder
-            .speak (answer + reprompt)
-            .reprompt (reprompt)
-            .withShouldEndSession (false)
-            .getResponse ()
-    },
-}*/
-
-const IngredientIntentHandler = {
-    canHandle(handlerInput) {
-        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-            && handlerInput.requestEnvelope.request.intent.name === 'IngredientIntent'
-    },
-    async handle(handlerInput) {
-        const drink = handlerInput.requestEnvelope.request.intent.slots.strDrink.value
-        const recipe = await getDrinkRecipe(drink, 'strMeasure + strIngredient')
-
-        const answer = `Sounds good! Here is what you need to make${drink}. You will need ${recipe}.`
-        const reprompt = 'Do you need the ingredients for another drink?'
-
-        return handlerInput.responseBuilder
-            .speak(answer + reprompt)
-            .reprompt(reprompt)
-            .withShouldEndSession(false)
-            .getResponse()
-    },
-}
 const CancelandStopIntentHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'IntentRequest'
@@ -112,8 +59,8 @@ const HelpIntentHandler = {
             && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent'
     },
     handle(handlerInput) {
-        const guidance = 'MyBartender can get you a drink recipe or the ingredients for a drink.  For example, "MyBartender, how do I make an old-fashioned?"'
-        const reprompt = 'What recipe can I teach you?'
+        const guidance = 'Insult Me can do just that.  Insult you.  So stop being a wimp and ask for an insult!!'
+        const reprompt = 'can you handle an insult?'
 
         return handlerInput.responseBuilder
             .speak(guidance + reprompt)
@@ -122,25 +69,93 @@ const HelpIntentHandler = {
             .getResponse()
     }
 }
+
+const FallbackIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.FallbackIntent'
+    },
+    handle(handlerInput) {
+        const guidance = "I'm sorry, I did not get that.  are you ready to be insulted?"
+        const reprompt = 'Please, let me insult you!'
+
+        return handlerInput.responseBuilder
+            .speak(guidance + reprompt)
+            .reprompt(reprompt)
+            .withShouldEndSession(false)
+            .getResponse()
+    }
+}
+
 const ErrorHandler = {
     canHandle() {
         return true
     },
-    handle() {
-        const errorMessage = 'It appears our kegs are tapped and we are out of liquor!  Please try again later.'
+    handle(handlerInput) {
         return handlerInput.responseBuilder
-            .speak(errorMessage)
+            .speak('It appears I have turned a new leaf.  I will not insult you, but try me later!')
             .withShouldEndSession(true)
+    }
+}
+
+const NavigateHomeIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.NavigateHomeIntent'
+    },
+    handle(handlerInput) {
+        const homeText = "Looking for something to bring you down a peg?  You have come to the right place!  Would you like an insult?"
+        const reprompt = 'I am begging you, let me dis you!'
+
+        return handlerInput.responseBuilder
+            .speak(homeText + reprompt)
+            .reprompt(reprompt)
+            .withShouldEndSession(false)
             .getResponse()
     }
 }
-const skillBuilder = Alexa.SkillBuilders.custom()
+
+const NoIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
+            handlerInput.requestEnvelope.request.intent.name === 'AMAZON.NoIntent'
+    },
+    handle(handlerInput) {
+        const speechText = 'had enough?  come back later if you dare!'
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withShouldEndSession(true)
+            .getResponse()
+    },
+}
+
+const YesIntent = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
+            handlerInput.requestEnvelope.request.intent.name === 'AMAZON.YesIntent'
+    },
+    async handle(handlerInput) {
+        const insult = await getInsult()
+
+        return handlerInput.responseBuilder
+            .speak(`Here is your insult: <break strength='strong' />${insult}<break strength='strong' /> Would you like another dis?`)
+            .reprompt('Would you like more words from the genius?')
+            .withShouldEndSession(false)
+            .getResponse()
+    },
+}
 
 exports.handler = skillBuilder.addRequestHandlers(
-    RecipeIntentHandler,
-    IngredientIntentHandler,
+    NavigateHomeIntentHandler,
+    InsultIntentHandler,
     CancelandStopIntentHandler,
-    ErrorHandler,
     HelpIntentHandler,
     FallbackIntentHandler,
-).lambda()
+    LaunchRequestHandler,
+    YesIntent,
+    NoIntentHandler
+)
+    .addErrorHandler(ErrorHandler)
+    .lambda()
+
